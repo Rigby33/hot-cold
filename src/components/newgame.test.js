7613cb2d3e6c0b8d6375a708f7c219265cffd2cc
Catherine Lynn <catherineannlynn@gmail.com>
2018-03-01 22:40:19 -0500

    correcting typo in a test

--- src/components/newgame.test.js
+++ src/components/newgame.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import NewGame from './NewGame';
+import NewGame from './newgame';
 
 describe('<NewGame/>', () => {
 	it('Renders without crashing', () => {
